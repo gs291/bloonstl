1664377fd423c2e1b2b9bcd492db7281358e832f
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-02-28 02:03:41 -0800

    Move global styles into _app.js.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,32 @@
 import { Provider } from "react-redux";
+import { Global, css } from "@emotion/react";
 
 import store from "../lib/redux/store";
-import '../styles/globals.css';
+
+const globals = css` 
+  html, 
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  
+  * {
+    box-sizing: border-box;
+  }
+`;
 
 function App({ Component, pageProps }) {
   return (
       <Provider store={store}>
-        <Component {...pageProps} />
+          <Global styles={ globals } />
+          <Component {...pageProps} />
       </Provider>
   );
 }
