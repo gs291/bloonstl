08dd15aa9d8a3eaafca0adba508de149a9e83d12
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-02-27 19:16:40 -0800

    Add new pages

--- pages/index.js
+++ pages/index.js
@@ -11,5 +11,5 @@ export default function App() {
                 </Page>
             </StylesProvider>
         </>
-  )
+  );
 }
---
+++ pages/tierlist.js
@@ -0,0 +1,11 @@
+import Page from "../components/Page";
+
+export default function TierList() {
+    return (
+        <>
+            <Page title="Tier List">
+                <div>Tier List</div>
+            </Page>
+        </>
+    );
+}
