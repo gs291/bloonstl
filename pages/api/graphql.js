0c205f708d2c493f9551ea4c0ec0ba39201f9f7a
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-03-01 15:18:49 -0800

    Refactor database/graphql code

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,44 +1,10 @@
-import mysql from "serverless-mysql";
+
 import { ApolloServer } from 'apollo-server-micro';
 
-import MonkeysAPI from "../../lib/graphql/datasources/monkeys";
-import HeroesAPI from "../../lib/graphql/datasources/heroes";
-import AbilitiesAPI from "../../lib/graphql/datasources/abilities";
-
+import { dataSources, context } from "../../lib/mysql/db";
 import typeDefs from "../../lib/graphql/schema";
 import resolvers from "../../lib/graphql/resolvers";
 
-const db = mysql({
-    client: 'mysql',
-    version: '5.7',
-    config: {
-        host : process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        user : process.env.MYSQL_USERNAME,
-        password : process.env.MYSQL_PASSWORD,
-        database : process.env.MYSQL_DATABASE
-    }
-});
-
-async function executeQuery({ query, values }) {
-    try {
-        const results = await db.query(query, values);
-        await db.end();
-        return results;
-    } catch (error) {
-        return { error };
-    }
-}
-
-const dataSources = () => ({
-    monkeysAPI: new MonkeysAPI({ executeQuery }),
-    heroesAPI: new HeroesAPI({ executeQuery }),
-    abilitiesAPI: new AbilitiesAPI({ executeQuery }),
-});
-
-const context = () => {
-    return {};
-}
 
 const apolloServer = new ApolloServer(
     {
