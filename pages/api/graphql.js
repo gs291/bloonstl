de4268593f0f920259577ea73209ea42182b0444
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-03-01 14:06:22 -0800

    Add graphql api endpoint.   - Correctly serves and access MySQL database elements with GraphQL.

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,5 +1,57 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import mysql from "serverless-mysql";
+import { ApolloServer } from 'apollo-server-micro';
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+import MonkeysAPI from "../../lib/datasources/monkeys";
+import HeroesAPI from "../../lib/datasources/heroes";
+import AbilitiesAPI from "../../lib/datasources/abilities";
+
+import typeDefs from "../../lib/graphql/schema";
+import resolvers from "../../lib/graphql/resolvers";
+
+const db = mysql({
+    client: 'mysql',
+    version: '5.7',
+    config: {
+        host : process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        user : process.env.MYSQL_USERNAME,
+        password : process.env.MYSQL_PASSWORD,
+        database : process.env.MYSQL_DATABASE
+    }
+});
+
+export async function executeQuery({ query, values }) {
+    try {
+        const results = await db.query(query, values);
+        await db.end();
+        return results;
+    } catch (error) {
+        return { error };
+    }
 }
+
+const dataSources = () => ({
+    monkeysAPI: new MonkeysAPI({ executeQuery }),
+    heroesAPI: new HeroesAPI({ executeQuery }),
+    abilitiesAPI: new AbilitiesAPI({ executeQuery }),
+});
+
+const context = () => {
+    return {};
+}
+
+const apolloServer = new ApolloServer(
+    {
+        typeDefs,
+        resolvers,
+        dataSources,
+        context
+    });
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+}
+
+export default apolloServer.createHandler({ path: '/api/graphql' });
