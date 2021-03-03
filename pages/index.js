535e5d3ce2b157b6463b4ca68ade18391f351a0b
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-03-02 21:18:18 -0800

    Remove old styles inject

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
-import { StylesProvider } from "@material-ui/core/styles";
-
 import Page from "../components/Page";
 
 export default function Home() {
     return (
         <>
-            <StylesProvider injectFirst>
-                <Page title="test">
-                    <div>okay?</div>
-                </Page>
-            </StylesProvider>
+            <Page title="test">
+                <div>okay?</div>
+            </Page>
         </>
   );
 }
