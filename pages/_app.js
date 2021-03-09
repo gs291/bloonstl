21ff410f8a43eab248ad30d17e3eb4fb9c7cf155
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-03-09 14:49:34 -0800

    Update scrollbar styling

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,22 @@ const globals = css`
   * {
     box-sizing: border-box;
   }
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+  
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+  
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+  }
+  
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
 `;
 
 export default function App({ Component, pageProps }) {
