86c1b2693468e54e2c0af7a923637513b5e94d8c
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-03-17 03:13:16 -0700

    Fetch and display all monkeys on monkeys page

--- pages/monkeys.js
+++ pages/monkeys.js
@@ -1,9 +1,43 @@
-import Page from "../components/Page";
+import {dataSources} from "../lib/mysql/db";
+import MonkeysGrid from "../components/monkey/MonkeysGrid";
 
-export default function Monkeys() {
+
+export default function Monkeys({ monkeys }) {
+    const primary = JSON.parse(monkeys.primary);
+    const military = JSON.parse(monkeys.military);
+    const magic = JSON.parse(monkeys.magic);
+    const support = JSON.parse(monkeys.support);
+
     return (
         <>
-            <div>Monkeys</div>
+            <MonkeysGrid primary={primary} military={military} magic={magic} support={support} />
         </>
     );
+}
+
+export async function getStaticProps(context) {
+    let monkeys = await dataSources().monkeysAPI.getAllMonkeys();
+
+    let primary = [], military = [], magic = [], support = [];
+    monkeys.forEach(monkey => {
+        if (monkey.type === "Primary") {
+            primary.push(monkey);
+        } else if (monkey.type === "Military") {
+            military.push(monkey);
+        } else if (monkey.type === "Magic") {
+            magic.push(monkey);
+        } else  {
+            support.push(monkey);
+        }
+    });
+    return {
+        props: {
+            monkeys: {
+                "primary": JSON.stringify(primary),
+                "military": JSON.stringify(military),
+                "magic": JSON.stringify(magic),
+                "support": JSON.stringify(support)
+            }
+        }
+    }
 }
