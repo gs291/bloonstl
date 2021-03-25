499c1c3932413dbb4a280f850f69cec6a94b03b4
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-03-24 21:35:17 -0700

    Update tower Icon component to (hopefully) not lazy-load tower images

--- components/tower/Icon.js
+++ components/tower/Icon.js
@@ -5,29 +5,39 @@ import {
 import { getImageUrl } from "../../lib/utils";
 
 const CardMediaContainer  = styled(CardMedia)`
-  height: 165px;
-  width: 128px;
   background-size: contain;
 `;
 
 const IconContainer  = styled.div`
   margin-right: 10px;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   
   width: ${props => props.detailed ? "" : "100%"};
 `;
 
+const ImgContainer = styled.div`
+  height: 165px;
+  width: 128px;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
 export default function Icon({ className, tower, detailed }) {
     return (
         <>
             <IconContainer detailed={detailed} className={className}>
-                <CardMediaContainer
-                    image={ getImageUrl(tower.filename) }
-                    title={ tower.name }
-                />
+                <ImgContainer>
+                    <CardMediaContainer
+                        image={ getImageUrl(tower.filename) }
+                        title={ tower.name }
+                        component="img"
+                        priority="true"
+                    />
+                </ImgContainer>
             </IconContainer>
         </>
     );
