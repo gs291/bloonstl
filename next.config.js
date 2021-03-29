a7e746fe2a9fe611392da81498e8ad913756cf5c
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-03-29 13:28:25 -0700

    Add configuraiton for cloudinary image service

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    images: {
+        loader: 'cloudinary',
+        path: 'https://res.cloudinary.com/bloons/image/upload/',
+        deviceSizes: [640, 960, 1080, 3840]
+    },
+}
