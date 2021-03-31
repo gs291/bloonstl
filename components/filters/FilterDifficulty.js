3b07055ef8bf8cd6708cd6e5baf6c7ee4a1044f8
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-03-30 19:56:24 -0700

    Update responsiveness for difficulty filter

--- components/filters/FilterDifficulty.js
+++ components/filters/FilterDifficulty.js
@@ -5,6 +5,9 @@ import {FormLabel,
     Radio,
     RadioGroup} from "@material-ui/core";
 
+import {useSelector} from "react-redux";
+import {getMobile} from "../../lib/redux/selectors";
+
 const FormRow = styled.div`
   display: flex;
   flex-direction: row;
@@ -15,18 +18,32 @@ const FilterLabel = styled(FormLabel)`
 `;
 
 export default function FilterDifficulty({ className, difficulty, handleDifficulty }) {
-
+    const mobile = useSelector(getMobile);
     return (
         <>
             <FormControl component="fieldset">
                 <FilterLabel focused={false}>Difficulty Prices:</FilterLabel>
                 <RadioGroup value={difficulty} onChange={handleDifficulty}>
-                    <FormRow>
-                        <FormControlLabel value="easy" control={<Radio size="small"/>} label="Easy" />
-                        <FormControlLabel value="medium" control={<Radio size="small" />} label="Medium" />
-                        <FormControlLabel value="hard" control={<Radio size="small"/>} label="Hard" />
-                        <FormControlLabel value="impoppable" control={<Radio size="small"/>} label="Impoppable" />
-                    </FormRow>
+                    { !mobile && (
+                        <FormRow>
+                            <FormControlLabel value="easy" control={<Radio size="small"/>} label="Easy" />
+                            <FormControlLabel value="medium" control={<Radio size="small" />} label="Medium" />
+                            <FormControlLabel value="hard" control={<Radio size="small"/>} label="Hard" />
+                            <FormControlLabel value="impoppable" control={<Radio size="small"/>} label="Impoppable" />
+                        </FormRow>
+                    )}
+                    { mobile && (
+                        <>
+                            <FormRow>
+                                <FormControlLabel value="easy" control={<Radio size="small"/>} label="Easy" />
+                                <FormControlLabel value="medium" control={<Radio size="small" />} label="Medium" />
+                            </FormRow>
+                            <FormRow>
+                                <FormControlLabel value="hard" control={<Radio size="small"/>} label="Hard" />
+                                <FormControlLabel value="impoppable" control={<Radio size="small"/>} label="Impoppable" />
+                            </FormRow>
+                        </>
+                    )}
                 </RadioGroup>
             </FormControl>
         </>
