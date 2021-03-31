575a6acf497c4ed8717b9d4debde07e0dd9fe3d8
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-03-31 11:36:22 -0700

    Update hero page to use vertical divider component

--- components/hero/HeroContainer.js
+++ components/hero/HeroContainer.js
@@ -2,7 +2,8 @@ import styled from "@emotion/styled";
 import {Divider, Typography} from "@material-ui/core";
 
 import Icon from "../tower/Icon";
-import {goldCost} from "../../lib/utils";
+import {getHeroColor, goldCost} from "../../lib/utils";
+import VerticalDivider from "../divider/VerticalDivider";
 
 const ContentContainer = styled.div`
   display: flex;
@@ -14,18 +15,13 @@ const MonkeyText = styled.div`
   flex-direction: column;
 `;
 
-const VerticalDivider = styled(Divider)`
-  margin-left: 10px;
-  margin-right: 10px;
-`;
-
 export default function HeroContainer({hero, difficulty}) {
 
     return (
         <>
             <ContentContainer>
                 <Icon tower={ hero } />
-                <VerticalDivider flexItem orientation="vertical" />
+                <VerticalDivider backgroundColor={getHeroColor(hero.name)} />
                 <MonkeyText>
                     <Typography variant="h3">
                         {hero.name}
