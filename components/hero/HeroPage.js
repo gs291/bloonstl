a9c4922cbd1dd7b4a5f39fd2a0b1179281e6e36e
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-03-31 16:51:25 -0700

    Remove old difficulty filter with useState

--- components/hero/HeroPage.js
+++ components/hero/HeroPage.js
@@ -10,17 +10,13 @@ import TowerContainer from "../tower/TowerContainer";
 
 export default function HeroPage({ hero }) {
     const mobile = useSelector(getMobile);
-    const [ difficulty, setDifficulty ] = useState("medium");
-
-    const handleDifficulty = (event) => setDifficulty(event.target.value);
-
     const dividerBackgroundColor = getHeroColor(hero.name);
 
     return (
         <>
             <TowerContainer tower={hero} towerType="hero" />
             <FixedDivider width={ mobile ? 100 : 80 } backgroundColor={dividerBackgroundColor}/>
-            <FilterDifficulty difficulty={difficulty} handleDifficulty={ handleDifficulty }/>
+            <FilterDifficulty />
             <FixedDivider width={ mobile ? 100 : 80 } backgroundColor={dividerBackgroundColor}/>
             <HeroDetailed hero={hero} />
         </>
