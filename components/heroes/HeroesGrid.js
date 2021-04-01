b1d68be45d078b2463ec6566cdbe223b0ab285e8
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-04-01 01:00:07 -0700

    Add title bar to heroes page

--- components/heroes/HeroesGrid.js
+++ components/heroes/HeroesGrid.js
@@ -4,7 +4,15 @@ import {Grid} from "@material-ui/core";
 import TowerCard from "../tower/TowerCard";
 import {useSelector} from "react-redux";
 import {getMobile} from "../../lib/redux/selectors";
+import {getMonkeyTypeColor} from "../../lib/utils";
 
+const HeroTitle = styled(Grid)`
+  padding-top: 5px;
+  padding-bottom: 5px;
+  
+  background-color: #004A7D;
+  color: #000;
+`;
 
 export default function HeroesGrid({ heroes }) {
     const mobile = useSelector(getMobile);
@@ -16,13 +24,20 @@ export default function HeroesGrid({ heroes }) {
 
     return (
         <>
-            <Grid container spacing={2}>
-                { heroes.map(hero => (
-                        <Grid xs={gridSpacing} item key={hero.id}>
-                            <TowerCard tower={hero} towerType="hero"/>
-                        </Grid>
-                    )
-                )}
+            <Grid direction="column" container spacing={2}>
+                <HeroTitle item>
+                    Heroes
+                </HeroTitle>
+                <Grid item>
+                    <Grid container spacing={2}>
+                        { heroes.map(hero => (
+                                <Grid xs={gridSpacing} item key={hero.id}>
+                                    <TowerCard tower={hero} towerType="hero"/>
+                                </Grid>
+                            )
+                        )}
+                    </Grid>
+                </Grid>
             </Grid>
         </>
     );
