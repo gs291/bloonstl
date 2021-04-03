df8dd34544f9f8b620bfe0ab5cbe1f6474731d20
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-04-03 12:46:51 -0700

    Update import ordering

--- components/navbar/MenuButton.js
+++ components/navbar/MenuButton.js
@@ -4,9 +4,9 @@ import MenuIcon from "@material-ui/icons/Menu";
 import ClearIcon from "@material-ui/icons/Clear";
 import {useDispatch, useSelector} from "react-redux";
 
+import {siteSizes} from "../../lib/utils";
 import {getDrawer} from "../../lib/redux/selectors";
 import { toggleDrawer } from "../../lib/redux/actions";
-import {siteSizes} from "../../lib/utils";
 
 const Menu = styled(Button)`
   color: white;
