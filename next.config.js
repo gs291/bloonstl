3bb3840cf92b13ad71d00010bbd4bb9724ed712d
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-04-02 21:23:25 -0700

    Update cloudinary deviceSizes to only 960px

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     images: {
         loader: 'cloudinary',
         path: 'https://res.cloudinary.com/bloons/image/upload/',
-        deviceSizes: [640, 960, 1080, 3840]
+        deviceSizes: [960]
     },
 }
