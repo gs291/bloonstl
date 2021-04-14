80b0c1ca9b79370848f80db15e8a166fa61ba148
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-04-13 23:40:56 -0700

    Update FilterRanks to be form friendly

--- components/filters/FilterRanks.js
+++ components/filters/FilterRanks.js
@@ -10,23 +10,24 @@ const BGroup = styled(ButtonGroup)`
 
 
 const RankButton  = styled(Button)`
-  background-color: ${props => rankColors(props.name)};
+  background-color: ${props => rankColors(props.value)};
   font-family: ${font_family};
   
   &:hover,
   &:active,
   &:focus {
-    background-color: ${props => rankColors(props.name, true)};
+    background-color: ${props => rankColors(props.value, true)};
   }
 `;
 
 export default function FilterRanks({ className, handleRank }) {
+    if (!handleRank){ handleRank = (_, __) => <></>; }
     return (
         <>
             <BGroup variant="contained" disableElevation>
-                <RankButton onClick={(e) => handleRank(e, "s")} name="s">S&nbsp;&nbsp;Tier</RankButton>
-                <RankButton onClick={(e) => handleRank(e, "a")} name="a">A&nbsp;&nbsp;Tier</RankButton>
-                <RankButton onClick={(e) => handleRank(e, "b")} name="b">B&nbsp;&nbsp;Tier</RankButton>
+                <RankButton onClick={(e) => handleRank(e, "s")} name="rank" value="s">S&nbsp;&nbsp;Tier</RankButton>
+                <RankButton onClick={(e) => handleRank(e, "a")} name="rank" value="a">A&nbsp;&nbsp;Tier</RankButton>
+                <RankButton onClick={(e) => handleRank(e, "b")} name="rank" value="b">B&nbsp;&nbsp;Tier</RankButton>
             </BGroup>
         </>
     );
