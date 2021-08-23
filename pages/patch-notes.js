20dafc4bb45deac89c09449a70d07c8dc3ebdd56
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-08-23 15:08:28 -0700

    Add initial revision of patch-notes page

---
+++ pages/patch-notes.js
@@ -0,0 +1,18 @@
+import Header from "../components/page/Header";
+import PageTitle from "../components/page/PageTitle";
+import MainContainer from "../components/page/MainContainer";
+
+export default function PatchNotes({  }) {
+
+    return (
+        <>
+            <Header title="Patch Notes" />
+            <MainContainer>
+                <PageTitle title="Patch Notes" />
+                <div>
+                    Patch Notes
+                </div>
+            </MainContainer>
+        </>
+    );
+}
