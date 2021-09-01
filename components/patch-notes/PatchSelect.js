8887c69cac2eac39b49b063348090c32bb57b756
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-09-01 00:38:07 -0700

    Add all patch notes from v 21.0-27.2

--- components/patch-notes/PatchSelect.js
+++ components/patch-notes/PatchSelect.js
@@ -19,9 +19,13 @@ export default function PatchSelect({ patch, handlePatchSelect }) {
                     <MenuItem value={"27.0"}>v 27.0</MenuItem>
                     <MenuItem value={"26.1"}>v 26.1</MenuItem>
                     <MenuItem value={"26.0"}>v 26.0</MenuItem>
+                    <MenuItem value={"25.1"}>v 25.1</MenuItem>
                     <MenuItem value={"25.0"}>v 25.0</MenuItem>
+                    <MenuItem value={"24.2"}>v 24.2</MenuItem>
+                    <MenuItem value={"24.1"}>v 24.1</MenuItem>
                     <MenuItem value={"24.0"}>v 24.0</MenuItem>
                     <MenuItem value={"23.0"}>v 23.0</MenuItem>
+                    <MenuItem value={"21.1"}>v 21.1</MenuItem>
                     <MenuItem value={"21.0"}>v 21.0</MenuItem>
                 </Select>
             </FormControl>
