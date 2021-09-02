01f65f3c9f6241b164ff08b57e06d68a4a1c82dc
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-09-01 18:22:58 -0700

    Add text shadow to patch note towers

--- components/patch-notes/PatchItems.js
+++ components/patch-notes/PatchItems.js
@@ -1,23 +1,44 @@
 import styled from "@emotion/styled";
+import {useSelector} from "react-redux";
 
 import TowerText from "../tower/TowerText";
+import siteColors from "../../lib/utils/siteColors";
+import {getDarkMode} from "../../lib/redux/selectors";
+import {getHeroColor, getMonkeyColor} from "../../lib/utils/utils";
 
 const ItemList = styled.li`
   margin-top: 20px;
+  
+  &::marker {
+    color: ${props => props["data-dm"] ? siteColors.text.dark : siteColors.text.light};
+  }
+`;
+
+const TowerTitle = styled(TowerText)`
+  ${props => props["data-bc"] ? `text-shadow: 0 5px 10px ${props["data-bc"]};` : ''}
 `;
 
 export default function PatchItems({ className, items }) {
+    const darkMode = useSelector(getDarkMode);
 
     return (
         <>
             <ul>
                 {items.map((item, idx) => (
-                    <ItemList key={idx}>
-                        <TowerText variant={item.text ? "h6" : "h5"} font={!!item.text}>
+                    <ItemList key={idx} data-dm={darkMode}>
+                        <TowerTitle
+                            variant={item.text ? "h6" : "h5"}
+                            font={!!item.text}
+                            data-bc={
+                                item.tower ?
+                                    item.type ? getMonkeyColor(item.type) : getHeroColor(item.tower)
+                                    : null
+                            }
+                        >
                             {item.text ?
                                 item.text
                                 : item.tower ? item.tower : item.title}
-                        </TowerText>
+                        </TowerTitle>
                         {item.description && (
                             <TowerText variant="body1" font={true}>
                                 {item.description}
