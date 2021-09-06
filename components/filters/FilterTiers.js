f734b0db912332806d1c881bdae5f3145c8f3616
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-09-06 13:57:03 -0700

    Remove unused code

--- components/filters/FilterTiers.js
+++ components/filters/FilterTiers.js
@@ -1,11 +1,9 @@
 import styled from "@emotion/styled";
 import {useSelector} from "react-redux";
-import {ButtonGroup, Button, Typography} from "@material-ui/core";
+import {ButtonGroup} from "@material-ui/core";
 
 import TierButton from "./TierButton";
-import siteColors from "../../lib/utils/siteColors";
 import {getDarkMode} from "../../lib/redux/selectors";
-import {font_family, getTierCardColor, getTierColor} from "../../lib/utils/utils";
 
 const BGroup = styled(ButtonGroup)`
   justify-content: center;
