57b270e0fdacd1c98777ab0a30a4d5bcb6eee8db
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-09-15 20:50:39 -0700

    Update resources GitHub text sizes

--- components/resources/GitHub.js
+++ components/resources/GitHub.js
@@ -33,13 +33,13 @@ export default function GitHub({className, tag}) {
                     GitHub Information
                 </Title>
 
-                <Text variant="h5" font={true}>
+                <Title variant="h5" font={true}>
                     This project is Open Source!
-                </Text>
-                <Text variant="body1" font={true}>
+                </Title>
+                <Text variant="h6" font={true}>
                     The project was created with a powerful React framework, Next.js, that incorporates a multitude of services such as redux, GraphQL, MySQL, etc.
                 </Text>
-                <Text variant="body1" font={true}>
+                <Text variant="h6" font={true}>
                     To view or contribute to this project you can locate it at:&nbsp;
                     <TextLink href="https://github.com/gs291/bloonstl">bloonstl</TextLink>
                 </Text>
