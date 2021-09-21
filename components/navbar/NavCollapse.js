e7e7c9d26acea31d97df943b3fd397aee763fdb7
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-09-21 14:25:24 -0700

    Fix error with NavCollapse ClickAwayListener props. Remove unused code

--- components/navbar/NavCollapse.js
+++ components/navbar/NavCollapse.js
@@ -1,14 +1,14 @@
 import styled from "@emotion/styled";
-import {useRef, useState} from "react";
+import {useState} from "react";
 import {useSelector} from "react-redux";
-import {ClickAwayListener, Menu, MenuItem, Popover, Popper} from "@material-ui/core";
+import {ClickAwayListener, MenuItem, Popover} from "@material-ui/core";
 
 import NavLink from "./NavLink";
 import {rgbaHex} from "../../lib/utils/utils";
+import TextTooltip from "../tooltip/TextTooltip";
 import siteSizes from "../../lib/utils/siteSizes";
 import siteColors from "../../lib/utils/siteColors";
 import {getDarkMode, getMobile} from "../../lib/redux/selectors";
-import TextTooltip from "../tooltip/TextTooltip";
 
 const NavExpandText = styled.div`
   height: 10px;
@@ -52,10 +52,6 @@ const ExpandMenu = styled(Popover)`
   }
 `;
 
-const MenuContainer = styled.div`
-
-`;
-
 const NavExpandItem = styled(MenuItem)`
   width: 100%;
 
@@ -65,25 +61,11 @@ const NavExpandItem = styled(MenuItem)`
   align-items: center;
 `;
 
-const MoreTooltip = styled(TextTooltip)`
-  ${props => props["data-d"] ? "" : "display: none;" }
-`;
-
 export default function NavCollapse({links, expanderRef, anchorEl, handleExpand, handleClose}){
     const [isHover, setIsHover] = useState(false);
 
     const mobile = useSelector(getMobile);
     const darkMode = useSelector(getDarkMode);
-
-    const Expander = () => (
-        <NavExpand onClick={handleExpand} ref={expanderRef} data-m={mobile} data-dm={darkMode}>
-            <NavExpandHover>
-                <NavExpandText>
-                    . . .
-                </NavExpandText>
-            </NavExpandHover>
-        </NavExpand>
-    )
 
     return (
         <>
@@ -126,13 +108,13 @@ export default function NavCollapse({links, expanderRef, anchorEl, handleExpand,
                 hideBackdrop
             >
                 <ClickAwayListener onClickAway={handleClose}>
-                    <MenuContainer>
+                    <div>
                         {links.map(link => (
                             <NavExpandItem onClick={handleClose} key={link.key}>
                                 <NavLink {...link} />
                             </NavExpandItem>
                         ))}
-                    </MenuContainer>
+                    </div>
                 </ClickAwayListener>
             </ExpandMenu>
         </>
