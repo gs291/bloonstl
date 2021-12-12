954b78b95fa5e595478b86efc35409f0b264a104
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-12-11 18:43:52 -0800

    Update header Google Analytics script to have minimum white space

--- components/page/Header.js
+++ components/page/Header.js
@@ -24,13 +24,13 @@ export default function Header({ title }) {
 
                 {/*// <!-- Global site tag (gtag.js) - Google Analytics --> */}
                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-ERWW6617M8" />
-                <script>{`
-                    window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-
-                        gtag('config', 'G-ERWW6617M8');
-                `}
+                <script>
+                    {
+                        `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-ERWW6617M8');`
+                    }
                 </script>
 
                 {/*// <!-- Google Adsense --> */}
