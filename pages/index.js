ad299a6778f2d7c309f00c7da683bfabb6ded1cc
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-12-13 12:36:40 -0800

    Remove unused code

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Header from "../components/page/Header";
 import HomePage from "../components/index/HomePage";
 import TopSection from "../components/index/TopSection";
 import MainContainer from "../components/page/MainContainer";
-import HorizontalAD from "../components/advertisment/HorizontalAD";
 
 export default function Home({ monkeys }) {
     const tierSection = useRef(null);
