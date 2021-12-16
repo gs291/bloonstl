428c6d8b7854772c97fb98fd9f83d35bc5e0b425
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-12-15 18:30:02 -0800

    Update footer to include links to legal pages

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import styled from "@emotion/styled";
 import {useSelector} from "react-redux";
 import {Typography} from "@mui/material";
+import {Link as MUILink} from "@mui/material";
 
 import siteSizes from "../../lib/utils/siteSizes";
 import siteColors from "../../lib/utils/siteColors";
 import {getDarkMode} from "../../lib/redux/selectors";
 import {font_family, rgbaHex} from "../../lib/utils/utils";
+import NavLink from "../navbar/NavLink";
 
 const FooterContainer = styled.footer`
   display: flex;
@@ -13,6 +16,14 @@ const FooterContainer = styled.footer`
   justify-content: center;
   align-items: center;
   height: ${siteSizes.footer.height};
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 `;
 
 const FooterText = styled(Typography)`
@@ -38,12 +49,53 @@ const FooterLink = styled.a`
   }
 `;
 
+const MLink = styled(MUILink)`
+  margin-left: 10px;
+  margin-right: 10px;
+  color: ${props => props["data-dm"] ? siteColors.text.dark : siteColors.text.light};
+  font-size: 1.25em;
+
+  &:hover {
+    cursor: pointer;
+    color: ${props => props["data-dm"] ? siteColors.text.navLink.dark : siteColors.text.navLink.light};
+  }
+`;
+
+const Dot = styled.div`
+  width: 5px; 
+  height: 5px; 
+  border-radius: 5px; 
+  margin-left: 8px; 
+  margin-right: 8px; 
+  background-color: ${props => props["data-dm"] ? siteColors.accent.dark : siteColors.accent.light};;
+`;
+
+const links = [
+    { "text": "Terms", "path": "/legal/terms-of-service", "dot": true },
+    { "text": "Privacy", "path": "/legal/privacy-policy", "dot": true },
+    { "text": "Cookie", "path": "/legal/cookie-policy" }
+]
+
 export default function Footer({ className }) {
     const darkMode = useSelector(getDarkMode);
 
     return (
         <>
             <FooterContainer className={className}>
+                <LinkContainer>
+                    {links.map(link => (
+                        <LinkContainer key={link.text}>
+                            <Link href={link.path} prefetch={false} passHref>
+                                <MLink underline="none" data-dm={darkMode}>
+                                    {link.text}
+                                </MLink>
+                            </Link>
+                            {link.dot && (
+                                <Dot data-dm={darkMode}/>
+                            )}
+                        </LinkContainer>
+                    ))}
+                </LinkContainer>
                 <FooterText component="span" variant="body1">
                     Written and maintained by
                     <FooterLink href="https://gregsanchez.netlify.app/" data-dm={darkMode}>
