395d2b2c85a8b7908c154530ac26e114dd37a588
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-12-17 15:24:01 -0800

    Update patch note sections to fill full width of parent container

--- components/patch-notes/Patch.js
+++ components/patch-notes/Patch.js
@@ -4,6 +4,10 @@ import PatchItems from "./PatchItems";
 import TowerText from "../tower/TowerText";
 import FixedDivider from "../divider/FixedDivider";
 
+const PatchContainer = styled.div`
+  width: 100%;
+`;
+
 const PatchTitle = styled(TowerText)`
   margin-top: 30px;
 `;
@@ -13,13 +17,13 @@ export default function Patch({ patch, tags }) {
     return (
         <>
             {patch.items.map((patchItem, idx) => (
-                <div id={tags[idx] && tags[idx].href} key={patchItem.title}>
+                <PatchContainer id={tags[idx] && tags[idx].href} key={patchItem.title}>
                     <PatchTitle variant="h3">
                         {patchItem.title}
                     </PatchTitle>
                     <PatchItems items={patchItem.items} />
                     <FixedDivider width={100}/>
-                </div>
+                </PatchContainer>
             ))}
         </>
     );
