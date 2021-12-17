2ee06ba45b708b9673b3d222d0bd1c9238782941
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-12-17 13:17:33 -0800

    Refactor entire patch notes select to now dynamically fetch all patches from the server

--- components/patch-notes/PatchNotesPage.js
+++ components/patch-notes/PatchNotesPage.js
@@ -1,11 +1,17 @@
 import styled from "@emotion/styled";
+import {useSelector} from "react-redux";
 import {useEffect, useState} from "react";
+import {CircularProgress} from "@mui/material";
 
 import Patch from "./Patch";
 import PatchDate from "./PatchDate";
 import PatchSelect from "./PatchSelect";
-import {getTowerLink} from "../../lib/utils/utils";
+import TowerText from "../tower/TowerText";
+import siteColors from "../../lib/utils/siteColors";
+import {getDarkMode} from "../../lib/redux/selectors";
 import {patchVersions} from "../../lib/utils/patches";
+import {fetchAPI, getTowerLink} from "../../lib/utils/utils";
+import patchQueries from "../../lib/graphql/queries/patchQueries";
 import TableOfContents from "../table-of-contents/TableOfContents";
 
 const PageContainer = styled.div`
@@ -23,33 +29,132 @@ const Date = styled(PatchDate)`
   margin-top: 50px;
 `;
 
+const Loading = styled.div`
+  margin-top: 30px;
+  margin-bottom: 30px;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+const ErrorContainer = styled.div`
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
+
 export default function PatchNotesPage({ patch }) {
-    const [patchVersion, setPatchVersion] = useState(patchVersions[0]);
-    const [patchData, setPatchData] = useState(patch);
+    const darkMode = useSelector(getDarkMode);
+
     const [toc, setToc] = useState([]);
+    const [patchVersion, setPatchVersion] = useState(patchVersions[0]);
+    const [patchData, setPatchData] = useState({[patchVersions[0]]: patch});
+
+    const [progress, setProgress] = useState({
+        isLoading: false,
+        isError: false,
+        errorMessages: []
+    });
 
     const handlePatchSelect = (e) => {
-        setPatchVersion(e.target.value); //TODO: FETCH DATA INSTEAD OF USING HUGE JSON VARIABLE
+        if (!patchData[e.target.value]) {
+            setPatchData(prevState => ({...prevState, [e.target.value]: {"release": "1900-01-01", "items": []}}));
+        }
+        setPatchVersion(e.target.value);
     }
 
-    useEffect(() => {
-        const tags = patchData.items.map(item => {
-            return {
-                "href": getTowerLink(item.title),
-                "title": item.title
-            }
-        });
+    useEffect( () => {
+        let ignore = {"ignore": false};
+        const controller = new AbortController();
+        const query = patchQueries.queryPatchByVersion;
+        const variables = { version: patchVersion };
 
-        setToc(tags);
+        const fetchData = async () => {
+            const responseBody = await fetchAPI(query, variables, controller, ignore, setProgress);
+
+            if (!responseBody.errors && responseBody.data && responseBody.data.patchByVersion) {
+                setPatchData(prevState => { return { ...prevState, [patchVersion]: responseBody.data.patchByVersion }});
+            }
+        }
+
+        if (patchData[patchVersion] && patchData[patchVersion].release === "1900-01-01") {
+            fetchData();
+        }
+
+        if (patchData[patchVersion]) {
+            const tags = patchData[patchVersion].items.map(item => {
+                return {
+                    "href": getTowerLink(item.title),
+                    "title": item.title
+                }
+            });
+            setToc(tags);
+        }
+
+        return () => {
+            controller.abort();
+            ignore.ignore = true;
+        };
+    }, [patchVersion])
+
+    useEffect(() => {
+        if (patchData[patchVersion] && patchData[patchVersion].release !== "1900-01-01") {
+            const tags = patchData[patchVersion].items.map(item => {
+                return {
+                    "href": getTowerLink(item.title),
+                    "title": item.title
+                }
+            });
+            setToc(tags);
+        }
     }, [patchData])
+
 
     return (
         <>
             <PageContainer>
                 <Select patch={patchVersion} handlePatchSelect={handlePatchSelect} />
-                <TableOfContents tags={toc} />
-                <Date date={patchData.release} />
-                <Patch patch={patchData} tags={toc}/>
+
+                {progress.isLoading && (
+                    <Loading>
+                        <TowerText variant="h4">
+                            Loading
+                        </TowerText>
+                        <CircularProgress />
+                    </Loading>
+                )}
+                {(progress.isError || progress.errorMessages.length > 0) && (
+                    <ErrorContainer>
+                        <TowerText
+                            variant="h4"
+                            textColor={darkMode ? siteColors.error.dark : siteColors.error.light }
+                        >
+                            {progress.errorMessages && progress.errorMessages.length > 1 ? (
+                                "Errors have Occurred"
+                            ) : (
+                                "An Error Has Occurred"
+                            )}
+                        </TowerText>
+                        {progress.errorMessages && progress.errorMessages.map(err => (
+                            <TowerText
+                                variant="h6"
+                                font={true}
+                                key={err.message}
+                            >
+                                {err.message}
+                            </TowerText>
+                        ))}
+                    </ErrorContainer>
+                )}
+                {!progress.isLoading && !progress.isError && (
+                    <>
+                        <TableOfContents tags={toc} />
+                        <Date date={patchData[patchVersion].release} />
+                        <Patch patch={patchData[patchVersion]} tags={toc}/>
+                    </>
+                )}
             </PageContainer>
         </>
     );
