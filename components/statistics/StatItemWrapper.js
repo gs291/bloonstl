bcf99ecead876b7d9cab6ff66ae98043bd846a31
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-12-30 22:10:42 -0800

    Update stat tooltips for damage, pierce, range, attack speed, projectiles, duration, delay and income

--- components/statistics/StatItemWrapper.js
+++ components/statistics/StatItemWrapper.js
@@ -19,6 +19,31 @@ const TextTooltip = ({text}) => (
     </>
 );
 
+const TextWithPreviousTooltip = ({text, value, prevValue}) => (
+    <>
+        <TowerText variant="body1" font={true}>
+            {text}
+        </TowerText>
+        {(typeof value === "number" && typeof prevValue === "number") && ((value - prevValue) !== 0) && (
+            <>
+                <FixedDivider />
+                <TowerText variant="body1" font={true}>
+                    +&nbsp;{prevValue.toFixed(2)}&nbsp;(Base)
+                </TowerText>
+                <TowerText variant="body1" font={true}>
+                    {(value - prevValue) > 0 ? '+' : '-'}
+                    &nbsp;
+                    {(value - prevValue) > 0 ? (value - prevValue).toFixed(3) : (-1 * (value - prevValue)).toFixed(3) }
+                    &nbsp;(Modifiers)
+                </TowerText>
+                <TowerText variant="body1" font={true}>
+                    = {value.toFixed(3)}
+                </TowerText>
+            </>
+        )}
+    </>
+);
+
 const critOccurrenceToText = (critOccurrence) => {
     if (critOccurrence === "0~0") {
         return 'Critical damage never occurs.'
@@ -77,11 +102,11 @@ const DamageText = (type, typeDamage, baseDamage, extraDamage = 0 ) => {
 
 const codeParser = (code, value, prevValue, initialDamage = 0, baseDamage = 0, extraDamage = 0) => {
     switch(code) {
-        case "Damage": return {"tooltip": <TextTooltip text={`Damage (${value}) is the amount of health a single hit of an attack removes.`} />};
-        case "Pierce": return {"tooltip": <TextTooltip text={`Pierce (${value}) (a.k.a Popping Power) is the number of different targets a single projectile can hit.`} />};
-        case "Range": return {"tooltip": <TextTooltip text={`Range (${value}) is the radius of the targetable area.`} />};
-        case "Atk Spd": return {"decimals": 3,"tooltip": <TextTooltip text={`Attack Speed (${value}) (a.k.a Reload time) is the number of seconds between attacks.`} />, "suffix": "&nbsp;s"};
-        case "Projectiles": return {"tooltip": <TextTooltip text={`Projectile Count (${value}) is the number of projectiles emitted at once.`} />};
+        case "Damage": return {"tooltip": <TextWithPreviousTooltip text={`Damage is the amount of health a single hit of an attack removes.`} value={value} prevValue={prevValue} />};
+        case "Pierce": return {"tooltip": <TextWithPreviousTooltip text={`Pierce (a.k.a Popping Power) is the number of different targets a single projectile can hit.`} value={value} prevValue={prevValue} />};
+        case "Range": return {"tooltip": <TextWithPreviousTooltip text={`Range is the radius of the targetable area.`} value={value} prevValue={prevValue} />};
+        case "Atk Spd": return {"decimals": 3,"tooltip": <TextWithPreviousTooltip text={`Attack Speed (a.k.a Reload time) is the number of seconds between attacks.`} value={value} prevValue={prevValue} />, "suffix": "&nbsp;s"};
+        case "Projectiles": return {"tooltip": <TextWithPreviousTooltip text={`Projectile Count is the number of projectiles emitted at once.`} value={value} prevValue={prevValue} />};
         case "Type": return {"counter": false, "tooltip": <TypesTooltip type={value} prevType={prevValue} />};
         case "Camo": return {"tooltip": DamageText("Camo", initialDamage, baseDamage)};
         case "Ceramic": return {"tooltip": DamageText("Ceramic", initialDamage, baseDamage)};
@@ -95,10 +120,10 @@ const codeParser = (code, value, prevValue, initialDamage = 0, baseDamage = 0, e
         case "Status":return {"tooltip": DamageText("Status", initialDamage, baseDamage)};
         case "Stun": return {"tooltip": DamageText("Stun", initialDamage, baseDamage)};
         case "Hotkey": return {"counter": false, "tooltip": <TextTooltip text={`Default keyboard hotkey keybind: ${value}`} />};
-        case "Duration": return {"tooltip": <TextTooltip text={`Duration the tower base attack lasts: ${value} seconds. (e.g. Banana Farm passive bananas)`} />, "suffix": "&nbsp;s"};
+        case "Duration": return {"tooltip": <TextWithPreviousTooltip text={`Duration the tower base attack lasts in seconds. (e.g. Banana Farm passive bananas)`} value={value} prevValue={prevValue} />, "suffix": "&nbsp;s"};
         case "Footprint": return {"counter": false, "tooltip": <TextTooltip text={`Size the tower takes up on the map: ${value}`} />};
-        case "Delay": return {"tooltip": <TextTooltip text={`Delay until the attack occurs: ${value} seconds.`} />, "suffix": "&nbsp;s"};
-        case "Income": return {"prefix": "+&nbsp;$", "tooltip": <TextTooltip text={`Money generated per round: +$${value}`} />};
+        case "Delay": return {"tooltip": <TextWithPreviousTooltip text={`Delay until the attack occurs in seconds`} value={value} prevValue={prevValue} />, "suffix": "&nbsp;s"};
+        case "Income": return {"prefix": "+&nbsp;$", "tooltip": <TextWithPreviousTooltip text={`Money generated per round`} value={value} prevValue={prevValue} />};
         case "Crit Rate": return {"counter": false, "tooltip": <TextTooltip text={critOccurrenceToText(value)} />};
         case "Cooldown": return {"tooltip": <TextTooltip text={`Cooldown timer: ${value} seconds`} />};
         case "P Impact": return {"tooltip": <TextTooltip text={`Pierce Impact: ${value}`} />};
