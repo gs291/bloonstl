921a515c6dba78398f7201dd53af6133068edfdc
Gregory Sanchez <sanchez.gregory886@gmail.com>
2022-01-12 19:42:55 -0800

    Update Paragon to be more mobile friendly

--- components/ability/Paragon.js
+++ components/ability/Paragon.js
@@ -1,13 +1,13 @@
-import {useState} from "react";
 import styled from "@emotion/styled";
 import {useSelector} from "react-redux";
+import {useEffect, useState} from "react";
 
 import Ability from "./Ability";
+import TowerText from "../tower/TowerText";
 import {rgbaHex} from "../../lib/utils/utils";
 import siteColors from "../../lib/utils/siteColors";
 import {globalOptions} from "../../lib/utils/emotionStyled";
 import {getDarkMode, getMobile} from "../../lib/redux/selectors";
-import TowerText from "../tower/TowerText";
 
 
 const ParagonContainer = styled("div", globalOptions)`
@@ -20,12 +20,14 @@ const ParagonContainer = styled("div", globalOptions)`
   align-items: center;
   
   transition: 0.3s;
-  background-color: ${props => props["data-dm"] ? siteColors.paragon.dark : siteColors.paragon.light };
+  border: 4px solid ${props => props["data-dm"] ? siteColors.paragon.dark : siteColors.paragon.light };
+  background-color: ${props => rgbaHex(props["data-dm"] ? siteColors.paragon.dark : siteColors.paragon.light, 0.5) };
   border-radius: 10px;
+  
+  position: relative;
   
   &:hover {
     cursor: pointer;
-    box-shadow: -5px 5px 5px ${props => rgbaHex(props["data-dm"] ? siteColors.paragon.dark : siteColors.paragon.light, 0.5)};
   }
 `;
 
@@ -37,15 +39,75 @@ const AbilityContainer = styled("div")`
   height: 100%;
 `;
 
+const BackgroundContainer = styled("div", globalOptions)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
+
+const BackgroundText = styled("div")`
+  width: 100%;
+  height: 100%;
+  
+  display: flex;
+  flex-direction: ${props => props["data-m"] ? "row" : "column"};
+  justify-content: center;
+  align-items: center;
+`;
+
+const SplitText = styled("div")`
+  width: ${props => props["data-m"] ? 40 : 100}%;
+  height: ${props => props["data-m"] ? 100 : 35}%;
+`;
+
+const LeftText = styled(SplitText)`
+  display: flex;
+  justify-content: ${props => props["data-m"] ? "end" : "center"};
+  align-items: ${props => props["data-m"] ? "center" : "center"};
+`;
+
+const RightText = styled(SplitText)`
+  display: flex;
+  justify-content: ${props => props["data-m"] ? "start" : "center"};
+  align-items: ${props => props["data-m"] ? "center" : "center"};
+`;
+
+const MiddleFiller = styled("div")`
+  width: ${props => props["data-m"] ? 20 : 100}%;
+  height: ${props => props["data-m"] ? 100 : 30}%;
+`;
+
+const SidewaysText = styled(TowerText)`
+  text-orientation: mixed;
+  writing-mode: ${props => props["data-m"] ? "initial" : "vertical-rl"};
+  opacity: ${props => props["data-dm"] ? 0.15 : 0.4};
+`;
+
+
 export default function Paragon({ability, fileName, tier, towerType, selected, ...rest}) {
     const mobile = useSelector(getMobile);
     const darkMode = useSelector(getDarkMode);
 
     const [open, setOpen] = useState(false);
+    const [touchTimeout, setTouchTimeout] = useState(false);
 
-    const handleClick = () => {setOpen(prevOpen => !prevOpen);}
+    const handleTouch = () => {setTouchTimeout(true); setOpen(true);};
+    const handleClick = () => {setOpen(prevOpen => !prevOpen);};
     const handleEnter = () => {setOpen(true);};
     const handleExit = () => {setOpen(false);};
+
+    useEffect(() => {
+        if (touchTimeout) {
+            const timer = setTimeout(() => {
+                setOpen(false);
+                setTouchTimeout(false);
+            }, 7000); //leaveTouchDelay from Tooltip
+
+            return () => clearTimeout(timer)
+        }
+    }, [touchTimeout]);
 
     return (
         <>
@@ -53,9 +115,25 @@ export default function Paragon({ability, fileName, tier, towerType, selected, .
                 onMouseEnter={handleEnter}
                 onMouseLeave={handleExit}
                 onClick={handleClick}
+                onTouchStart={handleTouch}
                 data-m={mobile} data-dm={darkMode}
                 {...rest}
             >
+                <BackgroundContainer>
+                    <BackgroundText data-m={mobile}>
+                        <LeftText data-m={mobile}>
+                            <SidewaysText variant="h4" data-m={mobile} data-dm={darkMode}>
+                                PARA
+                            </SidewaysText>
+                        </LeftText>
+                        <MiddleFiller data-m={mobile} />
+                        <RightText data-m={mobile}>
+                            <SidewaysText variant="h4" data-m={mobile} data-dm={darkMode}>
+                                GON
+                            </SidewaysText>
+                        </RightText>
+                    </BackgroundText>
+                </BackgroundContainer>
                 <AbilityContainer>
                     <Ability ability={ability} fileName={fileName} tier={tier} towerType={towerType} selected={selected} open={open}/>
                 </AbilityContainer>
